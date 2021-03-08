a803f5595ff2e165550a4f60744da953fe995835
Marcin Lenkowski <marcin@lenkowski.net>
2021-03-08 12:48:35 +0100

    Refactor base repository

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Core
 Escola LMS Core Package
+
+Package contains all basic classes and features used in Escola LMS, and it's obligatory to work with any other LMS package.
+
+## Users
+
+User model and migrations provided by this package are the simplest user model that may be used in LMS.
+In the real app, you should setup your own guard, that may extend `EscolaSoft\EscolaLms\Models\User` and use user traits from other packages.
---
+++ src/Models/User.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace EscolaLms\Core\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use Notifiable, HasRoles, HasApiTokens, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $protected = [];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'password_reset_token',
+    ];
+
+    protected $appends = [
+        'onboarding_completed',
+        'avatar_url',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'onboarding_completed' => 'boolean',
+    ];
+
+    /**
+     * Get the user's full name.
+     *
+     * @return string
+     */
+    public function getNameAttribute()
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+
+    public function getEmailVerifiedAttribute(): bool
+    {
+        return $this->hasVerifiedEmail();
+    }
+
+    public function getAvatarUrlAttribute(): ?string
+    {
+        return $this->path_avatar ? Storage::url('storage/users/' . $this->path_avatar) : null;
+    }
+}
