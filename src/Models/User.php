d534bccd10a97c655b3663a01bb473ae690a1ed9
Marcin Lenkowski <marcin@lenkowski.net>
2021-03-09 08:53:07 +0100

    User refactor WIP

--- src/Models/User.php
+++ src/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements MustVerifyEmail
      *
      * @var array
      */
-    protected $protected = [];
+    protected $fillable = ['*'];
 
     /**
      * The attributes that should be hidden for arrays.
