8a52a6bbc5edab17d69222e09525349034bf5336
Paweł Chołaj <pawel.cholaj@escola.pl>
2021-08-20 16:07:22 +0200

    Response methods proposed refactor

--- src/Http/Controllers/EscolaLmsBaseController.php
+++ src/Http/Controllers/EscolaLmsBaseController.php
@@ -2,9 +2,13 @@
 
 namespace EscolaLms\Core\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Response;
 
@@ -25,30 +29,50 @@ class EscolaLmsBaseController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function sendResponse($result, $message)
+    public function sendResponse(mixed $data, string $message, int $code = 200): JsonResponse
     {
-        return Response::json([
-            'success' => true,
-            'data'    => $result,
+        $body = [
+            'success' => $code >= 200 && $code < 300,
             'message' => $message,
-        ]);
+        ];
+        if (!is_null($data)) {
+            $body['data'] = $data;
+        }
+        return Response::json($body, $code);
     }
 
-    public function sendError($error, $code = 404)
+    public function sendError(string $error, int $code = 404): JsonResponse
     {
-        return Response::json([
-                'success' => false,
-                'message' => $error,
-            ],
-            $code
-        );
+        return $this->sendResponse(null, $error, $code);
     }
 
-    public function sendSuccess($message)
+    public function sendSuccess(string $message, int $code = 200): JsonResponse
+    {
+        return $this->sendResponse(null, $message, $code);
+    }
+
+    public function sendResponseForResource(JsonResource $resource, string $message = ''): JsonResponse
+    {
+        $request = request();
+        $wrappedResource = $resource->resource;
+        if ($wrappedResource instanceof LengthAwarePaginator) {
+            $meta = $wrappedResource->toArray();
+            unset($meta['data']);
+            return $this->sendResponseWithMeta($resource->toArray($request), $meta, $message);
+        }
+        if ($wrappedResource instanceof Model && $wrappedResource->wasRecentlyCreated) {
+            return $this->sendResponse($resource->toArray($request), $message, 201);
+        }
+        return $this->sendResponse($resource->toArray($request), $message);
+    }
+
+    public function sendResponseWithMeta(array $data, array $meta, string $message = '', int $code = 200): JsonResponse
     {
         return Response::json([
-            'success' => true,
+            'success' => $code >= 200 && $code < 300,
+            'data'    => $data,
+            'meta' => $meta,
             'message' => $message,
-        ]);
+        ], $code);
     }
 }
