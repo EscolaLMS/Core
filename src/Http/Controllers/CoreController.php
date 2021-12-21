0796caced1a105d6f951151a5c7442b3ec2c0420
Paweł Chołaj <pawel.cholaj@escolasoft.com>
2021-12-21 21:07:33 +0100

    Swagger

--- src/Http/Controllers/CoreController.php
+++ src/Http/Controllers/CoreController.php
@@ -21,6 +21,40 @@ use Illuminate\Support\Str;
 class CoreController extends EscolaLmsBaseController
 {
 
+    /**
+     * @OA\Get(
+     *      path="/api/core/packages",
+     *      summary="List installed escolalms packages",
+     *      tags={"Core Admin"},
+     *      security={
+     *          {"passport": {}},
+     *      },
+     *      description="List of installed escolalms packages with versions",
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\MediaType(
+     *              mediaType="application/json"
+     *          ),
+     *          @OA\Schema(
+     *              type="object",
+     *              @OA\Property(
+     *                  property="success",
+     *                  type="boolean"
+     *              ),
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="array",
+     *                  @OA\Items(@OA\Schema(type="string"))
+     *              ),
+     *              @OA\Property(
+     *                  property="message",
+     *                  type="string"
+     *              )
+     *          )
+     *      )
+     * )
+     */
     public function packages()
     {
         $escolaLmsPackagesWithVersions = array_reduce(
